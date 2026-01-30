701af5bc5ab1cdf05365d4d2d949f534f1b385ee
EdwardThePerson <etn567@gmail.com>
2026-01-30 23:25:27 +0000

    Settings and Back button
    The settings can be accesses now AND you can go back

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,9 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    public void GoToScene(string scene)
+    [SerializeField] private GameObject m_activePanel;
+
+    public void ChangeScene(string scene)
     {
         SceneManager.LoadScene(scene);
+    }
+
+    public void ChangePanel(GameObject m_newPanel)
+    {
+        m_activePanel.SetActive(false);
+        m_newPanel.SetActive(true);
+        m_activePanel = m_newPanel;
     }
 
     public void QuitGame()
