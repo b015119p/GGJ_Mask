63e16470a02ca3fb96ee981dddc5b6c09e420a08
EdwardThePerson <etn567@gmail.com>
2026-02-01 02:29:26 +0000

    Menus and Win screen
    You can win and you can also menu if you please

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -7,7 +7,6 @@ public class MenuManager : MonoBehaviour
     //This is the panel which should be active at first
     [SerializeField] private GameObject m_activePanel;
     [SerializeField] private GameObject m_pausePanel;
-    [SerializeField] private GameObject m_settingsPanel;
     [SerializeField] private bool m_pausable;
     InputSystem_Actions m_GameControls;
 
@@ -35,14 +34,6 @@ public class MenuManager : MonoBehaviour
         m_activePanel.SetActive(false);
         m_newPanel.SetActive(true);
         m_activePanel = m_newPanel;
-        if (m_activePanel == m_pausePanel || m_activePanel == m_settingsPanel)
-        {
-            Time.timeScale = 0f;
-        }
-        else
-        {
-            Time.timeScale = 1f;
-        }
     }
 
     //Quits the game
@@ -57,6 +48,34 @@ public class MenuManager : MonoBehaviour
         if (ctx.performed && m_pausable)
         {
             ChangePanel(m_pausePanel);
+            FreezeTime(true);
+            ChangePausability(false);
+        }
+    }
+
+    //Freezes time bro :finnadie:
+    public void FreezeTime(bool m_freeze)
+    {
+        if (m_freeze)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    //Toggles whether the player can pause or not
+    public void ChangePausability(bool m_pause)
+    {
+        if (m_pause)
+        {
+            m_pausable = true;
+        }
+        else
+        {
+            m_pausable = false;
         }
     }
 }
